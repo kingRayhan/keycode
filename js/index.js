7db7d72ad775c25240b925abde1539573080ef89
kingrayhan <rayhan095@gmail.com>
2018-02-12 09:54:33 +0600

    change style

--- js/index.js
+++ js/index.js
@@ -2,9 +2,13 @@
 
 var body = document.querySelector('body');
 var audio = document.querySelector('audio');
+var initNote = document.querySelector('.init-note');
+var display = document.querySelector('.display');
 body.addEventListener('keydown', function (e) {
 	var key = document.querySelector('.key');
 	var keyCode = document.querySelector('.keycode-display');
+	display.style.display = 'table';
+	initNote.style.display = 'none';
 	key.innerHTML = e.key;
 	keyCode.innerHTML = e.keyCode;
 	audio.play();
