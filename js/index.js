5ff9fec35d4c3eab79edff881e3dd24719a2aff2
kingrayhan <rayhan095@gmail.com>
2018-02-12 08:37:35 +0600

    add key press sound effect

--- js/index.js
+++ js/index.js
@@ -1,9 +1,12 @@
 'use strict';
 
 var body = document.querySelector('body');
+var audio = document.querySelector('audio');
 body.addEventListener('keydown', function (e) {
 	var key = document.querySelector('.key');
 	var keyCode = document.querySelector('.keycode-display');
 	key.innerHTML = e.key;
 	keyCode.innerHTML = e.keyCode;
+	audio.play();
+	audio.currentTime = 0;
 });
